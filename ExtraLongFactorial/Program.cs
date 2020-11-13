566f30ddc606743c4b800486acb828722835a76e
Hristiyan Stefanov <christian.stefanov@outlook.com>
2020-11-13 12:19:59 +0200

    add ExtraLongFac solution

---
+++ ExtraLongFactorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger result = Fac(n);
+            Console.WriteLine(result);
+        }
+
+        public static BigInteger Fac(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Fac(n - 1);
+        }
+    }
+}
