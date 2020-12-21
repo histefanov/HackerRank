ca5f6a37630c576ba101a684e06e654c17a0451c
Hristiyan Stefanov <christian.stefanov@outlook.com>
2020-12-21 22:38:00 +0200

    add problem

---
+++ Ratios/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Ratios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            double positivesRatio = arr.Where(x => x > 0).ToArray().Length * 1.0 / n;
+            double negativesRatio = arr.Where(x => x < 0).ToArray().Length * 1.0 / n;
+            double zerosRatio = 1 - (positivesRatio + negativesRatio);
+
+            Console.WriteLine($"{positivesRatio:F6}\n{negativesRatio:F6}\n{zerosRatio:F6}");
+        }
+    }
+}
