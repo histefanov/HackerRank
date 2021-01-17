ad7e03767142c074ebaa0599661db610abf82f74
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-01-17 23:49:35 +0200

    add task

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] candles = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            candles = candles.OrderByDescending(x => x).ToArray();
+
+            var result = 1;
+            var idx = 0;
+            bool areEqual = true;
+
+            while (areEqual && idx + 1 < candles.Length)
+            {
+                if (candles[idx] == candles[idx + 1])
+                {
+                    result += 1;
+                    idx += 1;
+                }
+                else
+                {
+                    areEqual = false;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
