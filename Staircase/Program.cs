6d0cb5bd4d2466956386866f4f96effd7ca08cf2
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-01-18 23:10:44 +0200

    add staircase task

---
+++ Staircase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new String(' ', n - i) + new String('#', i));
+            }
+        }
+    }
+}
