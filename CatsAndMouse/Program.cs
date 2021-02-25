bf420c33c247f285953bc23b16401480cbef71cc
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-25 21:19:22 +0200

    add

---
+++ CatsAndMouse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CatsAndMouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var catAPos = int.Parse(Console.ReadLine());
+            var catBPos = int.Parse(Console.ReadLine());
+            var mousePos = int.Parse(Console.ReadLine());
+
+            var catADistance = Math.Abs(mousePos - catAPos);
+            var catBDistance = Math.Abs(mousePos - catBPos);
+
+            if (catADistance < catBDistance)
+            {
+                Console.WriteLine("Cat A");
+            }
+            else if (catBDistance < catADistance)
+            {
+                Console.WriteLine("Cat B");
+            }
+            else
+            {
+                Console.WriteLine("Mouse C");
+            }
+        }
+    }
+}
