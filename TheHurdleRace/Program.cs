9b219dfa3b4308228fa4dade6a3bb4810bfefbb0
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-03-05 21:18:59 +0200

    add

---
+++ TheHurdleRace/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace TheHurdleRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hurdles = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var jumpingHeight = int.Parse(Console.ReadLine());
+
+            var highestHurdle = hurdles.Max();
+            var potionsRequired = highestHurdle - jumpingHeight;
+
+            Console.WriteLine(potionsRequired > 0 ? potionsRequired : 0);
+        }
+    }
+}
