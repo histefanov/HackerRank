d836c7eeff9a7c7ea43349efe5b563cb081e8199
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-03-08 20:47:23 +0200

    add

---
+++ DesignerPDFViewer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace DesignerPDFViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var heights = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var word = Console.ReadLine();
+
+            var largestLetterHeight = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var currentLetter = word[i];
+                var currentLetterHeight = heights[(int)currentLetter - 97];
+
+                if (currentLetterHeight > largestLetterHeight)
+                {
+                    largestLetterHeight = currentLetterHeight;
+                }
+            }
+
+            var selectionArea = word.Length * largestLetterHeight;
+            Console.WriteLine(selectionArea);
+        }
+    }
+}
