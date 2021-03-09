44b8f886ae045cc1f619f8ba7cd6e8246646485e
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-03-09 21:19:22 +0200

    add

---
+++ UtopianTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cycles = int.Parse(Console.ReadLine());
+            int height = 1;
+
+            for (int i = 0; i < cycles; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    height *= 2;
+                }
+                else
+                {
+                    height++;
+                }
+            }
+
+            Console.WriteLine(height);
+        }
+    }
+}
