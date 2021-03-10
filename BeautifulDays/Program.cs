3130c92322a6e5e6150a95b1d5da5a34f4b4758f
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-03-10 19:58:57 +0200

    add

---
+++ BeautifulDays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace BeautifulDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lo = int.Parse(Console.ReadLine());
+            int hi = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int beautifulDays = 0;
+
+            for (int i = lo; i <= hi; i++)
+            {
+                var reversed = int.Parse(i.ToString().Reverse().ToString());
+                
+                if ((i - reversed) % k == 0)
+                {
+                    beautifulDays++;
+                }
+            }
+
+            Console.WriteLine(beautifulDays);
+        }
+    }
+}
