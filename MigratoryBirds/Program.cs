ae541614eecdce0eac43e76fb438b1c78f011d96
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-03-24 18:02:34 +0200

    add

---
+++ MigratoryBirds/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var birdSightings = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var result = MigratoryBirds(birdSightings);
+            Console.WriteLine(result);
+        }
+
+        static int MigratoryBirds(List<int> birdSightings)
+        {
+            var occurences = new Dictionary<int, int>();
+
+            foreach (var sighting in birdSightings.OrderBy(x => x))
+            {
+                if (!occurences.ContainsKey(sighting))
+                {
+                    occurences.Add(sighting, 0);
+                }
+                occurences[sighting]++;
+            }
+
+            var highestCount = occurences
+                .OrderByDescending(x => x.Value)
+                .First().Key;
+
+            return highestCount;
+        }
+    }
+}
