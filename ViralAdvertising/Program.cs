536f6e93677e51fce4240006d13604c14ba624f8
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-04-15 23:21:06 +0300

    add

---
+++ ViralAdvertising/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nthDay = int.Parse(Console.ReadLine());
+
+            var outreachCount = GetPeopleReached(nthDay);
+
+            Console.WriteLine(outreachCount);
+        }
+
+        public static int GetPeopleReached(int nthDay)
+        {
+            int likesCount = 0;
+            int recipients = 5;
+
+            for (int i = 0; i < nthDay; i++)
+            {
+                likesCount += recipients / 2;
+                recipients = recipients / 2 * 3;
+            }
+
+            return likesCount;
+        }
+    }
+}
