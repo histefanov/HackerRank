6c5b153b5b890452ce391cd24abc45bfc1f5a34e
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-04-16 23:58:40 +0300

    add

---
+++ SaveThePrisoner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SaveThePrisoner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prisonersCount = int.Parse(Console.ReadLine());
+            int sweetsCount = int.Parse(Console.ReadLine());
+            int startingPosition = int.Parse(Console.ReadLine());
+
+            var result = saveThePrisoner(prisonersCount, sweetsCount, startingPosition);
+            Console.WriteLine(result);
+        }
+
+        static int saveThePrisoner(int n, int m, int s)
+        {
+            while (m > 0)
+            {
+                s++;
+                if (s == n)
+                {
+                    s = 1;
+                }
+                m--;
+            }
+
+            return s;
+        }
+    }
+}
