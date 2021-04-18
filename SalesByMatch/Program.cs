922aaae21fa4b1dabb43cedf0a970f7a3b688474
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-04-19 00:11:43 +0300

    add

---
+++ SalesByMatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace SalesByMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var matchingPairs = GetMatchingPairs(n, arr);
+            Console.WriteLine(matchingPairs);
+        }
+
+        private static object GetMatchingPairs(int n, int[] arr)
+        {
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            var orderedArr = arr.OrderBy(x => x).ToArray();
+            var matchCounter = 0;
+
+            for (int i = 0; i < n - 1; i+=2)
+            {
+                if (orderedArr[i] == orderedArr[i + 1])
+                {
+                    matchCounter++;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            return matchCounter;
+        }
+    }
+}
---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TowersOfHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int start = 1;
+            int end = 3;
+
+            Hanoi(n, start, end);
+        }
+
+        private static void Hanoi(int n, int start, int end)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(start + " -> " + end);
+                return;
+            }
+
+            int other = 6 - (start + end);
+            Hanoi(n - 1, start, other);
+            Console.WriteLine(start + " -> " + end);
+            Hanoi(n - 1, other, end);
+        }
+    }
+}
