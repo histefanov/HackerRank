bfff1c5541f9098aa95792fa43e29e245b8c6783
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-04-23 19:38:20 +0300

    refactor

--- DrawingBook/Program.cs
+++ DrawingBook/Program.cs
@@ -6,7 +6,20 @@ namespace DrawingBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            int result = PageCount(n, p);
+
+            Console.WriteLine(result);
+        }
+
+        static int PageCount(int n, int p)
+        {
+            int pageTurnsFromFront = p / 2;
+            int pageTurnsFromBack = n / 2 - p / 2;
+
+            return Math.Min(pageTurnsFromFront, pageTurnsFromBack);
         }
     }
 }
