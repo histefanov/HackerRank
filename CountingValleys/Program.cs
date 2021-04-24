564d6edaffadd5672cda1b0dbaad022b62cad24c
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-04-24 10:53:15 +0300

    add

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = int.Parse(Console.ReadLine());
+            string path = Console.ReadLine();
+
+            int valleysCount = CountValleys(steps, path);
+            Console.WriteLine(valleysCount);
+        }
+
+        public static int CountValleys(int steps, string path)
+        {
+            int level = 0;
+            int valleysCount = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (path[i] == 'U')
+                {
+                    if (++level == 0)
+                    {
+                        valleysCount++;
+                    }
+                }
+                else
+                {
+                    level--;
+                }
+            }
+
+            return valleysCount;
+        }
+    }
+}
