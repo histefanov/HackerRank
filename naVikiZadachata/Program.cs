fa7161be6a22ac65794ad060e8f023237916ae4a
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-04-26 20:24:33 +0300

    add

---
+++ naVikiZadachata/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace naVikiZadachata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var year = Console.ReadLine();
+            var holidaysCount = int.Parse(Console.ReadLine());
+            var hometownWeekendsCount = int.Parse(Console.ReadLine());
+
+            var sofiaWeekendsCount = 48 - hometownWeekendsCount;
+            var saturdayGamesInSofia = sofiaWeekendsCount * 0.75;
+            var sundayGamesAtHome = hometownWeekendsCount;
+            var holidayGamesCount = holidaysCount * (2 / 3.0);
+
+            var totalGamesPlayed = saturdayGamesInSofia + sundayGamesAtHome + holidayGamesCount;
+
+            if (year == "leap") totalGamesPlayed *= 1.15;
+
+            Console.WriteLine(Math.Floor(totalGamesPlayed));
+        }
+    }
+}
