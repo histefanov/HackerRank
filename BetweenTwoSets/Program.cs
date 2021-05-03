e4c2978ca089bac7aefd4519e4f01db971b224c8
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-05-03 18:03:49 +0300

    add

--- BetweenTwoSets/Program.cs
+++ BetweenTwoSets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace BetweenTwoSets
 {
@@ -6,7 +7,27 @@ namespace BetweenTwoSets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var arr1 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var arr2 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int result = GetTotalX(arr1, arr2);
+        }
+
+        private static int GetTotalX(int[] arr1, int[] arr2)
+        {
+            var totalX = 0;
+
+            for (int i = 1; i <= arr2.Max(); i++)
+            {
+
+            }
         }
     }
 }
