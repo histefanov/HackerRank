b7dfe1453a5e6db2ae7dcc9bb0fc9846ecba8fdf
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-05-15 15:41:42 +0300

    add

---
+++ MaxConsecutiveSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxConsecutiveSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxSum = MaxSubarraySum(arr);
+            Console.WriteLine(maxSum);
+        }
+
+        private static int MaxSubarraySum(int[] a)
+        {
+            var currentSum = a[0];
+            var maxSum = currentSum;
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                currentSum = Math.Max(a[i] + currentSum, a[i]);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
