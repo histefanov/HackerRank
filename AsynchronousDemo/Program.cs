e079270d30fd7b325e20eb49a7c395ffb21448fa
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-05-25 08:35:55 +0300

    add demo

---
+++ AsynchronousDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintEvenAndOddAsynchronously();
+        }
+
+        private static void PrintEvenAndOddAsynchronously()
+        {
+            var evenTask = Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i+=2)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+
+            var oddTask = Task.Run(() =>
+            {
+                for (int i = 1; i <= 99; i+=2)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+
+            Task.WaitAll(evenTask, oddTask);
+        }
+    }
+}
