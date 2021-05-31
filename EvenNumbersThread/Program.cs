9b6c49892f946cc5d0887fd9d54504bcc07cc33b
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-05-31 16:10:28 +0300

    add

---
+++ EvenNumbersThread/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace EvenNumbersThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lo = int.Parse(Console.ReadLine());
+            int hi = int.Parse(Console.ReadLine());
+
+            var thread = new Thread(() =>
+            {
+                PrintEvenNumbersInRange(lo, hi);
+            });
+
+            thread.Start();
+            thread.Join();
+
+            Console.WriteLine("Thread finished work");
+        }
+
+        private static void PrintEvenNumbersInRange(int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
---
+++ FirstDuplicate/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var firstDuplicate = GetFirstDuplicate(array);
+            Console.WriteLine(firstDuplicate);
+        }
+
+        private static int GetFirstDuplicateWithHashSet(int[] a)
+        {
+            var result = -1;
+            var seen = new HashSet<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (seen.Contains(a[i]))
+                {
+                    result = a[i];
+                    break;
+                }
+
+                seen.Add(a[i]);
+            }
+
+            return result;
+        }
+
+        private static int GetFirstDuplicate(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[Math.Abs(a[i] - 1)] < 0)
+                {
+                    return Math.Abs(a[i]);
+                }
+                else
+                {
+                    a[Math.Abs(a[i] - 1)] = -a[Math.Abs(a[i] - 1)];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
