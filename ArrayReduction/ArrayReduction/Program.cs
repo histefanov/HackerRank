3405a11f7815fa80120d20899092dcc144cb2277
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-08-31 10:33:49 +0300

    added solution with heap structure for array reduction problem

---
+++ ArrayReduction/ArrayReduction/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ArrayReduction
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using C5;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var reductionCost = ReduceArray(nums);
+            Console.WriteLine(reductionCost);
+        }
+
+        private static int ReduceArray(List<int> nums)
+        {
+            var heap = new IntervalHeap<int>();
+            heap.AddAll(nums);
+
+            var cost = 0;
+
+            while (heap.Count > 1)
+            {
+                var first = heap.DeleteMin();
+                var second = heap.DeleteMin();
+
+                cost += (first + second);
+
+                heap.Add(first + second);
+            }
+
+            return cost;
+        }
+    }
+}
